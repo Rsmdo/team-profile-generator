0fbd1fd986ba6df5294ebcd439ba9ff184a64d30
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-23 11:43:34 -0400

    created and tested employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,11 @@
 const Employee = require("../lib/Employee.js");
+
+test('create employee object with name id and contact', () => {
+    const employee = new Employee('Jerry', 60, 'jerrytom@gmail.com');
+    //make sure employee id dont start with 0 
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
