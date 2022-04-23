da16103214b6881b1f936e53a5323b7904dde19e
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-23 14:00:08 -0400

    finished all profiles

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -8,3 +8,10 @@ test('create intern obj', () => {
     
     expect(intern.school) .toEqual(expect.any(String));
 });
+
+test('gets school and role', () => {
+
+
+    expect(intern.inputSchool()).toEqual(expect.any(String));
+    expect(intern.role()).toEqual("Intern");
+});
