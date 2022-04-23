161ec4781cfeb53d7164374a543c6777116f89ec
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-23 11:30:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+
