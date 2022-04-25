db4efe806d4c9cb04f4f63d72485ce9eeb9ba03f
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-25 11:24:18 -0400

    added html template need to add functions for team cards

---
+++ src/generateHTML.js
@@ -0,0 +1,38 @@
+const addHtml = function (teamCards) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profiles</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-dark bg-dark mb-5 " id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    </html>
+  
+  
+  `;
+  }
+
+
+
+
+module.exports = generateHTML; 
